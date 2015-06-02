f6e029f48b853a6dbb625810e4b1c2ea73dadc8e
Ed-at-GSA <edward.boriso@gsa.gov>
2015-06-02 10:04:08 -0400

    output to temp folder for content

--- bash/html/makeCirclePage.sh
+++ bash/html/makeCirclePage.sh
@@ -13,16 +13,16 @@ descrips=$5
 
 $scriptsDirectory/html/createCircle.sh $scriptsDirectory/html/circles.middle $outputTempDirectory/circles1.middle circle1 'Organization POC' $pocs
 $scriptsDirectory/html/createCircle.sh $scriptsDirectory/html/circles.middle $outputTempDirectory/circles2.middle circle2 'Repository Descriptions' $descrips 
-cat $scriptsDirectory/html/circles.top > $outputFile
+#cat $scriptsDirectory/html/circles.top > $outputFile
 cat $outputTempDirectory/circles1.middle >> $outputFile
 cat $outputTempDirectory/circles2.middle >> $outputFile
 echo "<table>" >> $outputFile
 cat $outputDataDirectory/htmlstats.txt >> $outputFile
 echo "</table>" >> $outputFile
 
-cat $scriptsDirectory/html/circles.bottom >> $outputFile
+#cat $scriptsDirectory/html/circles.bottom >> $outputFile
 
 timestamp=`date +"%m/%d/%y at %H:%M"`
 
 echo "<br><small>Report Generated on $timestamp</small>" >> $outputFile
-echo "</body></html>" >> $outputFile
+#echo "</body></html>" >> $outputFile
