4c7e78292a6c43472adf51156a43d2545f01d9a8
Ed-at-GSA <edward.boriso@gsa.gov>
2015-06-19 22:04:57 -0400

    corrected params

--- bash/aws/driver/buildAll.sh
+++ bash/aws/driver/buildAll.sh
@@ -1,23 +1,15 @@
 echo -e "\n---------------enter $0---------------"
-if [[ ( -z $1 || -z $2 ) ]]; then
-  echo "Usage: buildAll.sh [rootDirectory] [buildToken] [refresh:optional(default false)]"
+if [[ ( -z $1 || -z $2 || -z $3 ) ]]; then
+  echo "Usage: buildAll.sh [rootDirectory] [buildToken] [refresh]"
 else
   rootDirectory=$1
   buildToken=$2
-  if [[ ( -z $3 ) ]]; then
-    refresh="false"
-  else
-    if [[ ! ( $3 = "true" || $3 = "false" ) ]]; then
-      refresh="false"
-    else
-      refresh=$4
-    fi
-  fi
+  refresh=$3
 
   if [[ ( $refresh = "true" ) ]]; then
     $rootDirectory/buildGitHubIndex.sh $buildToken $rootDirectory/control/getConfigElement.sh $rootDirectory/allConfigRefresh.txt true 2>&1 | tee $rootDirectory/alloutput.log
   else
-    $rootDirectory/buildGitHubIndex.sh $buildToken $rootDirectory/control/getConfigElement.sh $rootDirectory/allConfigCache.txt true 2>&1 | tee $rootDirectory/alloutput.log
+    $rootDirectory/buildGitHubIndex.sh $buildToken $rootDirectory/control/getConfigElement.sh $rootDirectory/allConfigCache.txt false 2>&1 | tee $rootDirectory/alloutput.log
   fi
   cp -R $rootDirectory/output/publish/all/ /var/www/html/
 fi
