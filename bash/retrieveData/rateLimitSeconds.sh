55efd676a5260f5eb874c686cbfe0792d0349c51
Ed-at-GSA <edward.boriso@gsa.gov>
2015-06-24 11:27:01 -0400

    cleanup of file after run

--- bash/retrieveData/rateLimitSeconds.sh
+++ bash/retrieveData/rateLimitSeconds.sh
@@ -2,4 +2,5 @@ curl -si -H "Authorization: token $1" https://api.github.com/rate_limit > ./limi
 limitup=`grep -m 1 reset ./limit.txt | awk '{print $2}'`
 current=`date +%s`
 diff=$((limitup-current))
+rm ./limit.txt
 echo "$diff"
