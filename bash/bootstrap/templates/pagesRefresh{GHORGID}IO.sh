e3ff5b5ef65dde84792135ab39bb7372f77ddd0e
Ed-at-GSA <edward.boriso@gsa.gov>
2015-07-06 14:25:41 -0400

    added agency passthrough

--- bash/bootstrap/templates/pagesRefresh{GHORGID}IO.sh
+++ bash/bootstrap/templates/pagesRefresh{GHORGID}IO.sh
@@ -1,3 +1,3 @@
 echo "########## refresh in $0 is set to $1"
 
-{ROOT}/bash/driver/buildAndDeploy.sh {ROOT}/bash {ROOT}/output {EMAIL} {READKEY} {PUSHKEY} github.com/{GHORGID}/github-federal-stats.git $1
+{ROOT}/bash/driver/buildAndDeploy.sh {ROOT}/bash {ROOT}/output {EMAIL} {READKEY} {PUSHKEY} github.com/{GHORGID}/github-federal-stats.git $1 $2
