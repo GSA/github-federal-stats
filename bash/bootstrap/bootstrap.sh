744f87449556d07e569dc190c39a36653a409a8e
Ed-at-GSA <edward.boriso@gsa.gov>
2015-07-06 11:16:15 -0400

    added renaming of directories

--- bash/bootstrap/bootstrap.sh
+++ bash/bootstrap/bootstrap.sh
@@ -5,6 +5,9 @@ else
   configFile=$1
   ROOT=`sed -ne "s/ROOT:.*/\0/p" $configFile | sed "s/ROOT://"`
   configReader=$ROOT/bash/control/getConfigElement.sh
+
+  echo "changing main script permissions to allow execution..."
+  find $ROOT/bash/ -name "*.sh" -print -exec chmod 755 {} \;
   
   READKEY=`$configReader $configFile READKEY`
   PUSHKEY=`$configReader $configFile PUSHKEY`
@@ -42,6 +45,21 @@ else
   echo "replacing localscripts values in script templates..."
   find $localScripts -type f -print0 | xargs -0 sed -i "s:{LOCALSCRIPTS}:$localScripts:g"
 
+  echo "updating .bashrc..."
+  echo "##### start of aliases added by bootstrap script #####" >> ~/.bashrc
+  cat $localScripts/bashrc.snippet >> ~/.bashrc
+  echo "##### end of aliases added by bootstrap script #####" >> ~/.bashrc
+  source ~/.bashrc
+  rm $localScripts/bashrc.snippet
+
+  echo "renaming script files..."
+  theFile="$localScripts/pushLocalTo{GHORGID}IO.sh"
+  `mv $theFile ${theFile//\{GHORGID\}/$GHORGID}`
+  theFile="$localScripts/pagesRefresh{GHORGID}IO.sh"
+  `mv $theFile ${theFile//\{GHORGID\}/$GHORGID}`
+
+  echo "changing local script permissions to allow execution..."
+  find $localScripts -name "*.sh" -print -exec chmod 755 {} \;
 fi
 
 echo -e "\n---------------exit $0---------------"
